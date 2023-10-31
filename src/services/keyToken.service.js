a98320efb54019687a670fc2eeabc9dba276c3f1
Hieu Minh Vu <hieuminhvuu@gmail.com>
2023-10-31 12:06:58 +0300

    Logout api shop

--- src/services/keyToken.service.js
+++ src/services/keyToken.service.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const keytokenModel = require("../models/keytoken.model");
+// const { Types: { ObjectId } } = require("mongoose");
 class KeyTokenService {
     static createKeyToken = async ({
         userId,
@@ -41,6 +42,16 @@ class KeyTokenService {
         } catch (error) {
             return error;
         }
+    };
+
+    static findByUserId = async (userId) => {
+        // return await keytokenModel.findOne({ user: new ObjectId(userId) }).lean();
+        return await keytokenModel.findOne({ user: userId });
+    };
+
+    static removeKeyById = async (id) => {
+        // return await keytokenModel.deleteOne({ _id: new ObjectId(id) });
+        return await keytokenModel.deleteOne(id);
     };
 }
 
