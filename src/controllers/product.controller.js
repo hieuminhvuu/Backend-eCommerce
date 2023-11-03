a4b24f81ebefaa83d5e6dec2f4fc1181c210cd64
Hieu Minh Vu <hieuminhvuu@gmail.com>
2023-11-04 00:41:26 +0300

    isDraft, isPublish, unPublish, search for Product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -5,7 +5,6 @@ const { SuccessResponse } = require("../core/success.response");
 
 class ProductController {
     createProduct = async (req, res, next) => {
-        console.log("vcl", req.user);
         new SuccessResponse({
             message: "Create new Product success!",
             metadata: await ProductService.createProduct(
@@ -17,6 +16,65 @@ class ProductController {
             ),
         }).send(res);
     };
+
+    publishProductByShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: "Publish Product by Shop success!",
+            metadata: await ProductService.publishProductByShop({
+                product_shop: req.user.userId,
+                product_id: req.params.id,
+            }),
+        }).send(res);
+    };
+
+    unPublishProductByShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: "Unpublish Product by Shop success!",
+            metadata: await ProductService.unPublishProductByShop({
+                product_shop: req.user.userId,
+                product_id: req.params.id,
+            }),
+        }).send(res);
+    };
+
+    // QUERY //
+    /**
+     * @description Get all Drafts for shop
+     * @param {Number} limit
+     * @param {Number} skip
+     * @return {JSON}
+     */
+    getAllDraftsForShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: "Get list Draft success!",
+            metadata: await ProductService.findAllDraftForShop({
+                product_shop: req.user.userId,
+            }),
+        }).send(res);
+    };
+
+    /**
+     * @description Get all Publishes for shop
+     * @param {Number} limit
+     * @param {Number} skip
+     * @return {JSON}
+     */
+    getAllPublishForShop = async (req, res, next) => {
+        new SuccessResponse({
+            message: "Get list Publish success!",
+            metadata: await ProductService.findAllPublishForShop({
+                product_shop: req.user.userId,
+            }),
+        }).send(res);
+    };
+
+    getListSearchProduct = async (req, res, next) => {
+        new SuccessResponse({
+            message: "Get list Search Product success!",
+            metadata: await ProductService.searchProduct(req.params),
+        }).send(res);
+    };
+    // END QUERY //
 }
 
 module.exports = new ProductController();
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -7,6 +7,13 @@ const {
     furniture,
 } = require("../models/product.model");
 const { BadRequestError } = require("../core/error.response");
+const {
+    findAllPublishForShop,
+    findAllDraftForShop,
+    publishProductByShop,
+    unPublishProductByShop,
+    searchProductByUser,
+} = require("../models/repositories/product.repo");
 
 class ProductFactory {
     static productRegistry = {}; //key-class
@@ -25,6 +32,32 @@ class ProductFactory {
             throw new BadRequestError(`Invalid Product Types ${type}`);
         return new productClass(payload).createProduct();
     }
+
+    // QUERY //
+    static async findAllDraftForShop({ product_shop, limit = 50, skip = 0 }) {
+        const query = { product_shop, isDraft: true };
+        return await findAllDraftForShop({ query, limit, skip });
+    }
+
+    static async findAllPublishForShop({ product_shop, limit = 50, skip = 0 }) {
+        const query = { product_shop, isPublished: true };
+        return await findAllPublishForShop({ query, limit, skip });
+    }
+    // END QUERY //
+
+    // PUT //
+    static async publishProductByShop({ product_shop, product_id }) {
+        return await publishProductByShop({ product_shop, product_id });
+    }
+
+    static async unPublishProductByShop({ product_shop, product_id }) {
+        return await unPublishProductByShop({ product_shop, product_id });
+    }
+
+    static async searchProduct({ keySearch }) {
+        return await searchProductByUser({ keySearch });
+    }
+    // END PUT //
 }
 
 // Define base product class
